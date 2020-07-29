22b125fc6a75d81c9c6579e1a291940a616d7860
caye13 <Caye1300@gmail.com>
2020-07-29 14:51:43 -0700

    Initial Commit

---
+++ BandSaniGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BandSaniGenerator
+//
+//  Created by Ck2 Jedi on 7/29/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
